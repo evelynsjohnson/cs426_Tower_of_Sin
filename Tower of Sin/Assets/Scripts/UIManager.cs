ae26d808647d17e49537e449c1823ece2be81b0e
mariotinoco517 <161102981+mariotinoco517@users.noreply.github.com>
2026-03-18 21:31:45 -0500

    Open Chest Functionality
    Created chest prefab that opens and closes on interaction. Opening grants 1 "potion" to player.

--- Tower of Sin/Assets/Scripts/UIManager.cs
+++ Tower of Sin/Assets/Scripts/UIManager.cs
@@ -10,25 +10,30 @@ public class UIManager : MonoBehaviour
     public int weapon;
 
     public TMP_Text PotionsText;
+
+    public int maxPotions;
     private int numPotions;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         weapon = 0;
         numPotions = 0;
-        PotionsText.text = "" + numPotions + "/5";
+        PotionsText.text = "" + numPotions + "/" + maxPotions;
     }
 
     // Update is called once per frame
-    void AddPotion()
+    public void AddPotion()
     {
-        numPotions++;
-        PotionsText.text = "" + numPotions + "/5";
+        if (numPotions < maxPotions)
+        {
+            numPotions++;
+            PotionsText.text = "" + numPotions + "/" + maxPotions;
+        }
     }
     void SubPotion()
     {
         numPotions--;
-        PotionsText.text = "" + numPotions + "/5";
+        PotionsText.text = "" + numPotions + "/" + maxPotions;
     }
 
     void changeWeapon(int newWeapon)
