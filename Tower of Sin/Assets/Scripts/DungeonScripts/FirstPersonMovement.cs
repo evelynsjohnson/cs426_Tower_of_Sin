808855e29aa3fe723f60d408a01d1893705b0f8e
Soham Hisabia <sohamhisabia7@gmail.com>
2026-04-07 12:49:46 -0500

    fix for jumping bug (trial)

--- Tower of Sin/Assets/Scripts/DungeonScripts/FirstPersonMovement.cs
+++ Tower of Sin/Assets/Scripts/DungeonScripts/FirstPersonMovement.cs
@@ -76,6 +76,8 @@ public class FirstPersonMovement : MonoBehaviour
 
     void Update()
     {
+        if (Time.timeScale == 0f) return;
+
         HandleInput();
 
         currentHorizontalInput = Input.GetAxisRaw("Horizontal");
